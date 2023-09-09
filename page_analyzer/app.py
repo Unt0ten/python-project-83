09ae26a75ae3c9bc36b613041ff01990473e29e0
Alexandr Sakhatov <sakhatoff@gmail.com>
2023-09-09 15:48:52 +0700

    step 2

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,9 +1,14 @@
-from flask import Flask
-
+from flask import Flask, render_template
+import jinja2
 
 app = Flask(__name__)
+my_loader = jinja2.ChoiceLoader([
+    app.jinja_loader,
+    jinja2.FileSystemLoader(['templates']),
+    ])
+app.jinja_loader = my_loader
 
 
 @app.route('/')
-def hello_world():
-    return 'Welcome to Flask!'
+def open_main():
+    return render_template('index.html')
