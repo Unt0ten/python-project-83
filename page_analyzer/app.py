370cf120a0b699a1d378f1467433460a44c5e728
Alexandr Sakhatov <sakhatoff@gmail.com>
2023-09-11 21:19:17 +0700

    step3

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
 import jinja2
+import psycopg2
+import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 my_loader = jinja2.ChoiceLoader([
@@ -7,8 +10,37 @@ my_loader = jinja2.ChoiceLoader([
     jinja2.FileSystemLoader(['templates']),
     ])
 app.jinja_loader = my_loader
+DATABASE_URL = os.getenv('DATABASE_URL')
+load_dotenv()
+
+try:
+    connection = psycopg2.connect(DATABASE_URL)
+    print('[INFO] Сonnection was successful!')
+    with connection.cursor() as cursor:
+        cursor.execute(
+            'SELECT version();'
+            )
+        print(f'Server version {cursor.fetchone()}')
 
 
-@app.route('/')
-def open_main():
-    return render_template('index.html')
+    @app.route('/')
+    def open_main():
+        return render_template('index.html')
+
+
+    @app.route('/urls/<id>')
+    def get_specific_data(id):
+        return render_template('specific_data.html')
+
+
+    @app.route('/urls')
+    def get_all_data():
+        return render_template('all_data.html')
+
+except Exception as ex:
+    print('[INFO] Error while working with PostgreSQL', ex)
+
+finally:
+    if connection:
+        connection.close()
+        print('[INFO] PostgeSQL connection closed')
