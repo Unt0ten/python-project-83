eea4b7c46507d615e2cc1a486424095481451c82
Alexandr Sakhatov <sakhatoff@gmail.com>
2023-09-19 20:29:03 +0700

    fix

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -18,7 +18,10 @@ from flask import (
     )
 import requests
 import os
+from dotenv import load_dotenv
 
+
+load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY')
 repo = DB()
